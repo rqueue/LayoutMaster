85b78d8b1b56aaea878421fe7505085e6030a55a
Ryan Quan <ryanhquan@gmail.com>
2015-04-17 00:57:15 -0700

    Add card example

--- LayoutTestApp/ViewController.m
+++ LayoutTestApp/ViewController.m
@@ -10,34 +10,79 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    UIView *h1 = [UIView new];
-    h1.backgroundColor = [UIColor brownColor];
-    UIView *h2 = [UIView new];
-    h2.backgroundColor = [UIColor whiteColor];
-    NSDictionary *hv = NSDictionaryOfVariableBindings(h1, h2);
+//    UIView *h1 = [UIView new];
+//    h1.backgroundColor = [UIColor brownColor];
+//    UIView *h2 = [UIView new];
+//    h2.backgroundColor = [UIColor whiteColor];
+//    NSDictionary *hv = NSDictionaryOfVariableBindings(h1, h2);
+//
+//    UIView *view1 = [[UIView alloc] init];
+//    view1.backgroundColor = [UIColor blueColor];
+//    UIView *view2 = [FlowMaster viewFromVisualFormats:@[@"[h1]",
+//                                                        @"[h2](10)"]
+//                                     variableBindings:hv];
+//    view2.backgroundColor = [UIColor greenColor];
+//    UIView *view3 = [[UIView alloc] init];
+//    view3.backgroundColor = [UIColor redColor];
+//    UIView *view4 = [[UIView alloc] init];
+//    view4.backgroundColor = [UIColor orangeColor];
+//    UIView *view5 = [[UIView alloc] init];
+//    view5.backgroundColor = [UIColor redColor];
+//    UIView *view6 = [[UIView alloc] init];
+//    view6.backgroundColor = [UIColor orangeColor];
+//
+//    NSDictionary *variables = NSDictionaryOfVariableBindings(view1, view2, view3, view4, view5, view6);
+//    UIView *containerView = [FlowMaster viewFromVisualFormats:@[@"[view1(23)][view2](45)",
+//                                                                @"[view3][view4(22)](90)",
+//                                                                @"[view5(60)][view6(100)](200)"]
+//                                             variableBindings:variables];
 
-    UIView *view1 = [[UIView alloc] init];
-    view1.backgroundColor = [UIColor blueColor];
-    UIView *view2 = [FlowMaster viewFromVisualFormats:@[@"[h1]",
-                                                        @"[h2](10)"]
-                                     variableBindings:hv];
-    view2.backgroundColor = [UIColor greenColor];
-    UIView *view3 = [[UIView alloc] init];
-    view3.backgroundColor = [UIColor redColor];
-    UIView *view4 = [[UIView alloc] init];
-    view4.backgroundColor = [UIColor orangeColor];
-    UIView *view5 = [[UIView alloc] init];
-    view5.backgroundColor = [UIColor redColor];
-    UIView *view6 = [[UIView alloc] init];
-    view6.backgroundColor = [UIColor orangeColor];
+    UIImageView *p = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Person"]];
+    p.backgroundColor = [UIColor clearColor];
 
-    NSDictionary *variables = NSDictionaryOfVariableBindings(view1, view2, view3, view4, view5, view6);
-    UIView *containerView = [FlowMaster viewFromVisualFormats:@[@"[view1(23)][view2](45)",
-                                                                @"[view3][view4(22)](90)",
-                                                                @"[view5(60)][view6(100)](200)"]
-                                             variableBindings:variables];
+    UILabel *title = [UILabel new];
+    title.font = [UIFont fontWithName:@"Arial-Bold" size:17.0];
+    title.text = @"Here is the title";
 
-    containerView.backgroundColor = [UIColor grayColor];
+    UILabel *sub = [UILabel new];
+    sub.font = [UIFont fontWithName:@"Arial" size:13.0];
+    sub.text = @"Subtext description here";
+
+    NSDictionary *tv = NSDictionaryOfVariableBindings(title, sub);
+    UIView *text = [FlowMaster viewFromVisualFormats:@[@"[title](21)",
+                                                       @"[sub]"]
+                                    variableBindings:tv];
+
+    UITextView *body = [UITextView new];
+    body.font = [UIFont fontWithName:@"Helvetica" size:15.0];
+    body.text = @"Here is some text that should be displayed in the body of this view. It should span multiple lines.";
+
+    UIButton *b1 = [[UIButton alloc] init];
+    [b1 setTitle:@"Agree" forState:UIControlStateNormal];
+    [b1 setTitleColor:[UIColor purpleColor] forState:UIControlStateNormal];
+    b1.backgroundColor = [UIColor whiteColor];
+    UIButton *b2 = [[UIButton alloc] init];
+    [b2 setTitle:@"Disagree" forState:UIControlStateNormal];
+    [b2 setTitleColor:[UIColor purpleColor] forState:UIControlStateNormal];
+    b2.backgroundColor = [UIColor whiteColor];
+    UIButton *b3 = [[UIButton alloc] init];
+    [b3 setTitle:@"Unsure" forState:UIControlStateNormal];
+    [b3 setTitleColor:[UIColor purpleColor] forState:UIControlStateNormal];
+    b3.backgroundColor = [UIColor whiteColor];
+
+    NSDictionary *cv = NSDictionaryOfVariableBindings(p, text, body, b1, b2, b3);
+    UIView *card = [FlowMaster viewFromVisualFormats:@[@"[p(48)][text](48)",
+                                                       @"[body]",
+                                                       @"[b1(80)][b2(80)][b3(80)](40)"]
+                                    variableBindings:cv];
+
+
+    UIView *containerView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 300.0, 218.0)];
+    containerView.translatesAutoresizingMaskIntoConstraints = NO;
+    containerView.backgroundColor = [UIColor whiteColor];
+    containerView.layer.cornerRadius = 3.0;
+    containerView.layer.borderWidth = 1.0;
+    containerView.layer.borderColor = [UIColor blackColor].CGColor;
     [self.view addSubview:containerView];
 
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:containerView
@@ -71,6 +116,18 @@
                                                           attribute:NSLayoutAttributeCenterY
                                                          multiplier:1.0
                                                            constant:0.0]];
+
+
+    [containerView addSubview:card];
+    [containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-15-[c]-15-|"
+                                                                          options:0
+                                                                          metrics:nil
+                                                                            views:@{@"c": card}]];
+    [containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-15-[c]-15-|"
+                                                                          options:0
+                                                                          metrics:nil
+                                                                            views:@{@"c": card}]];
+
 }
 
 - (void)viewDidAppear:(BOOL)animated {
